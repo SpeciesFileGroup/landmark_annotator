5ab185690df4a620732c99e46337378892dc139e
Brian Walters <brianw@pixotech.com>
2016-10-27 16:00:27 -0500

    Landmark entity fleshed out for use with React component

--- src/classes/test/LandmarkAnnotation.spec.js
+++ src/classes/test/LandmarkAnnotation.spec.js
@@ -1,5 +1,6 @@
 const expect = require('chai').expect;
 const LandmarkAnnotation = require('../LandmarkAnnotation');
+const Landmark = require('../Landmark');
 
 describe('LandmarkAnnotation', () => {
     it('should be a class', () => {
@@ -9,6 +10,16 @@ describe('LandmarkAnnotation', () => {
     it('should construct empty', () => {
         const la = new LandmarkAnnotation();
         assertDefault(la);
+    });
+
+    describe('landmarks', () => {
+        it('should construct Landmark classes with landmark data', () => {
+            const landmarks = [ {}, {} ];
+            const la = new LandmarkAnnotation({ landmarks });
+            la.landmarks.forEach(l => {
+                expect(l).to.be.instanceOf(Landmark);
+            })
+        });
     });
 });
 
