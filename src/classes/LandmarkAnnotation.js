939757dfb66b0b3bc4fe7bee81aece4cbf0e3f5c
Brian Walters <brianw@pixotech.com>
2016-10-27 16:17:25 -0500

    Landmark annotation viewmodel integration with landmark

--- src/classes/LandmarkAnnotation.js
+++ src/classes/LandmarkAnnotation.js
@@ -1,9 +1,24 @@
+const Landmark = require('./Landmark');
+
 class LandmarkAnnotation {
-    constructor() {
-        this.landmarks = [];
+    constructor(args = {}) {
+        const {
+            landmarks = []
+        } = args;
+
+        this.landmarks = landmarks.map(l => new Landmark(l));
         this.imageUrl = '';
         this.distancePerPixel = 0.01;
         this.distanceUnit = 'mm';
+    }
+
+    getViewmodel() {
+        const landmarks = this.landmarks.map(l => l.getViewmodel());
+        const imageUrl = this.imageUrl;
+        const distancePerPixel = this.distancePerPixel;
+        const distanceUnit = this.distanceUnit;
+
+        return { landmarks, imageUrl, distancePerPixel, distanceUnit };
     }
 }
 
--- src/classes/test/LandmarkAnnotation.spec.js
+++ src/classes/test/LandmarkAnnotation.spec.js
@@ -8,14 +8,54 @@ describe('LandmarkAnnotation', () => {
     });
 
     it('should construct empty', () => {
-        const la = new LandmarkAnnotation();
+        const la = new LandmarkAnnotation();
         assertDefault(la);
+    });
+
+    describe('viewmodel', () => {
+        it('should return a viewmodel for a web component', () => {
+            const la = new LandmarkAnnotation();
+            const actualVM = la.getViewmodel();
+
+            expect(actualVM).to.deep.equal({
+                landmarks: [],
+                imageUrl: ``,
+                distancePerPixel: 0.01,
+                distanceUnit: 'mm'
+            })
+        });
+
+        it('should return landmark viewmodels', () => {
+            const landmarks = [
+                {
+                    points: [
+                        [500, 500]
+                    ],
+                    title: 'Foo',
+                    color: `#339988`
+                }
+            ];
+            const la = new LandmarkAnnotation({landmarks});
+
+            const actualLandmarks = la.getViewmodel().landmarks;
+            expect(actualLandmarks[0]).to.deep.equal({
+                points: [
+                    {
+                        x: 500,
+                        y: 500
+                    }
+                ],
+                title: 'Foo',
+                color: '#339988'
+            });
+        });
     });
 
     describe('landmarks', () => {
         it('should construct Landmark classes with landmark data', () => {
-            const landmarks = [ {}, {} ];
-            const la = new LandmarkAnnotation({ landmarks });
+            const landmarks = [{}, {}];
+            const la = new LandmarkAnnotation({landmarks});
+            expect(la.landmarks.length).to.be.greaterThan(0);
             la.landmarks.forEach(l => {
                 expect(l).to.be.instanceOf(Landmark);
             })
